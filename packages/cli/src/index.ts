4e1090f2d92d534190a81295eac268c6df0ab9f9
wuxx <wuxiaoxi924@pingan.com.cn>
2022-04-18 19:12:01 +0800

    feat(cli): 增加index导出

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,2 @@
+export { default as TaroMiniRemoteChunkPlugin } from './plugins/taro-mini-remote-chunk-plugin'
+export { default as WebpackMiniRemoteChunkPlugin } from './plugins/webpack-mini-remote-chunk-plugin'
